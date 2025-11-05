9d0032c4fe8d6b85201087fb6290833d6eea9b9d
Timur <gunkotim3@gmail.com>
2025-11-05 21:03:35 +0700

    clean project

--- composeApp/src/androidMain/kotlin/org/example/project/data/AppContainer.android.kt
+++
@@ -1,9 +0,0 @@
-package org.example.project.data
-
-import org.example.project.data.auth.AuthRepository
-import org.example.project.data.auth.NetworkAuthRepository
-import org.example.project.data.auth.UserAuthPreferencesRepository
-
-actual fun createAuthRepository(userAuthPreferencesRepository: UserAuthPreferencesRepository): AuthRepository {
-    return NetworkAuthRepository(userAuthPreferencesRepository)
-}
